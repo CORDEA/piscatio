c15aad199a2f45afa90b24bf2b7314eca47ea715
Yoshihiro Tanaka <contact@cordea.jp>
2023-01-21 13:23:36 +0900

    launch the server

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+#[macro_use] extern crate rocket;
+
 use dotenv::dotenv;
 use std::env;
 
@@ -16,9 +18,21 @@ fn client_secret() -> String {
     return env::var("INSTAGRAM_CLIENT_SECRET").expect("Failed to get the client secret.");
 }
 
+#[get("/login?<code>")]
+fn login(code: &str) -> () {
+}
+
 fn main() {
     dotenv().ok();
     tauri::Builder::default()
+        .setup(|app| {
+            tauri::async_runtime::spawn(
+                rocket::build()
+                    .mount("/", routes![login])
+                    .launch()
+            );
+            Ok(())
+        })
         .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![client_id, client_secret])
         .run(tauri::generate_context!())
